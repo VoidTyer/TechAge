3300747c0aac65b3fc0879021934a8d65825657a
VoidTyer <10766315+VoidTyer@users.noreply.github.com>
2024-12-19 18:01:42 -0300

    Base Project

---
+++ src/client/java/io/github/voidtyer/techage/TechAgeClient.java
@@ -0,0 +1,10 @@
+package io.github.voidtyer.techage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TechAgeClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/io/github/voidtyer/techage/TechAgeDataGenerator.java
@@ -0,0 +1,11 @@
+package io.github.voidtyer.techage;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TechAgeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
